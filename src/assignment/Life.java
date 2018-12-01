53ee5fd3c50065fa30dcd71e0fb8ce55e3422614
ganer <ganeralkidz@gmail.com>
2018-11-30 23:04:27 -0500

    commit

--- src/assignment/Life.java
+++ src/assignment/Life.java
@@ -13,7 +13,6 @@ import javafx.scene.shape.Circle;
  * @author hanyalbouz
  */
 public class Life extends Circle{
-
     public Life(double centerX, double centerY, double radius, Paint fill) {
         super(centerX, centerY, radius, fill);
     }
